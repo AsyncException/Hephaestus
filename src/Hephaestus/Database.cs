eeed2437a73c568cc0a8b386e190d4afe7a7080c
Async Exception <tijmenlodders@outlook.com>
2024-09-25 19:25:58 +0200

    Corrected an incorrect assembly name

--- src/Hephaestus/Database.cs
+++ src/Hephaestus/Database.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using System.Reflection;
 
 namespace Hephaestus;
 
 public static class Database
 {
-    public static Action<DbContextOptionsBuilder> GetDbContextConfiguration(IHostApplicationBuilder builder) => options_builder => options_builder.UseSqlite(builder.Configuration.GetConnectionString("Default"), b => b.MigrationsAssembly("Async.Discord.Bot"));
+    public static Action<DbContextOptionsBuilder> GetDbContextConfiguration(IHostApplicationBuilder builder) =>
+        options_builder => options_builder.UseSqlite(builder.Configuration.GetConnectionString("Default"), b => b.MigrationsAssembly(Assembly.GetEntryAssembly()?.GetName().Name));
 }
