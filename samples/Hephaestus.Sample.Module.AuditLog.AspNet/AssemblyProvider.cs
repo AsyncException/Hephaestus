dee07126d00c0da0afcb4349104afda9c5dd531c
Async Exception <tijmenlodders@outlook.com>
2025-02-14 21:59:18 +0100

    Updated the samples to reflect the EntityFramework changes.

--- samples/Hephaestus.Sample.Module.AuditLog.AspNet/AssemblyProvider.cs
+++ samples/Hephaestus.Sample.Module.AuditLog.AspNet/AssemblyProvider.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.Reflection;
 
@@ -9,7 +11,12 @@ public class AssemblyProvider : IAssemblyProvider
     public Assembly Assembly { get; } = typeof(AssemblyProvider).Assembly;
 
     public void OptionalModules(IHostApplicationBuilder builder) {
-        builder.Services.AddDbContext<DatabaseContext>(Database.GetDbContextConfiguration(builder));
+        string connection_string = builder.Configuration.GetConnectionString("Default") ?? "Data Source=Application.db";
+        string assembly_name = Assembly.GetEntryAssembly()?.GetName().Name ?? throw new Exception("Cannot get the name of the EntryAssembly");
+
+        builder.Services.AddDbContext<DatabaseContext>(options => {
+            options.UseSqlite(connection_string, b => b.MigrationsAssembly(assembly_name));
+        });
     }
 
     public void OptionalDependencies(IHost host) {
--- samples/Hephaestus.Sample.Module.AuditLog.Hosting/AssemblyProvider.cs
+++ samples/Hephaestus.Sample.Module.AuditLog.Hosting/AssemblyProvider.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System.Reflection;
 
 namespace Hephaestus.Sample.Module.AuditLog.Hosting;
@@ -9,7 +12,12 @@ public class AssemblyProvider : IAssemblyProvider
     public Assembly Assembly { get; } = typeof(AssemblyProvider).Assembly;
 
     public void OptionalModules(IHostApplicationBuilder builder) {
-        builder.Services.AddDbContext<DatabaseContext>(Database.GetDbContextConfiguration(builder));
+        string connection_string = builder.Configuration.GetConnectionString("Default") ?? "Data Source=Application.db";
+        string assembly_name = Assembly.GetEntryAssembly()?.GetName().Name ?? throw new Exception("Cannot get the name of the EntryAssembly");
+
+        builder.Services.AddDbContext<DatabaseContext>(options => {
+            options.UseSqlite(connection_string, b => b.MigrationsAssembly(assembly_name));
+        });
     }
 
     public void OptionalDependencies(IHost host) {
